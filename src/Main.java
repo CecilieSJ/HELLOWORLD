5140443677188a19fd82145edcc922859887caac
CecilieSJ <ceci749q@stud.kea.dkk>
2022-09-08 16:06:54 +0200

    "First commit"

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Oi!");
+        System.out.println("Meu nome e Cecilie e meu sobrenome e Sjørup-Johnsen");
+        System.out.println("Tudo bem?");
+        System.out.println("Estou bem e voce");
+    }
+}
